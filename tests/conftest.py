de6b91c80f0901adfd9d32e3faeb84d341b9d9e1
Артамонов Роман <r.artamonov@dssl.ru>
2025-03-19 08:04:07 +0300

    Add files from qa guru repo

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,15 @@ import dotenv
 import pytest
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def envs():
     dotenv.load_dotenv()
 
-
-@pytest.fixture(scope="session")
-def app_url():
+@pytest.fixture(scope='session')
+def app_url() -> str:
     return os.getenv("APP_URL")
+
+@pytest.fixture(scope='session')
+def users_endpoint() -> str:
+    app_url = os.getenv("APP_URL")
+    return f'{app_url}/api/users'
