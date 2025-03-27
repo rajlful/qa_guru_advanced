8e42bbfa9183026b2f9ea3a68f2603f96fa87605
Артамонов Роман <r.artamonov@dssl.ru>
2025-03-27 15:58:32 +0300

    add app to docker

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
+import logging
 import os
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
 from app.database.engine import create_db_tables
-from routes import status, users
+from app.routes import status, users
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.info('On start up')
+    create_db_tables()
+    yield
+    logging.info('On shut down')
+
 
 host = os.getenv('HOST', default='127.0.0.1')
 port = os.getenv('APP_PORT', default=8080)
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
@@ -17,6 +28,4 @@ add_pagination(app)
 
 if __name__ == "__main__":
     import uvicorn
-    create_db_tables()
-    print(f'kek {type(port)}')
     uvicorn.run(app=app, host=host, port=port)
